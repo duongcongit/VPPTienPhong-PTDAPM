64386a679bcf850b8e7dee73985b17c0e37dc0fe
Duong Cong <duongcongit@gmail.com>
2023-04-10 12:53:45 +0700

    update database template

---
+++ database/database.sql
@@ -0,0 +1,107 @@
+/* ========= SQL Query for create table ========= */
+-- Custommer
+CREATE TABLE customers (
+    customerID int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    fullname varchar(255) NOT NULL,
+    address varchar(255) NOT NULL,
+    phone varchar(20) NOT NULL,
+    email varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    status int NOT NULL,
+    token varchar(255)
+);
+
+-- Employee
+CREATE TABLE employees (
+    employeeID int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    fullname varchar(255) NOT NULL,
+    gender varchar(255) NOT NULL,
+    phone varchar(20) NOT NULL,
+    email varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    status int NOT NULL
+);
+
+-- Admin
+CREATE TABLE admins (
+    adminID int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    adminName varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    username varchar(255) NOT NULL,
+    password varchar(255) NOT NULL
+);
+
+-- Category
+CREATE TABLE categories (
+    categoryID varchar(30) PRIMARY KEY NOT NULL,
+    name varchar(255) NOT NULL
+);
+
+-- Supplier
+CREATE TABLE suppliers (
+    supplierID int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    supplierName varchar(255) NOT NULL,
+    address varchar(255) NOT NULL,
+    phone varchar(20) NOT NULL,
+    email varchar(255) NOT NULL
+);
+
+-- Product
+CREATE TABLE products (
+    productID varchar(10) PRIMARY KEY NOT NULL,
+    productName varchar(255) NOT NULL, 
+    detail varchar(3000) NOT NULL,
+    stock int(20) NOT NULL,
+    sold int(20) NOT NULL,
+    price int(20) NOT NULL,
+    status int(5) NOT NULL,
+    categoryID varchar(30) NOT NULL,
+    supplierID int NOT NULL,
+    FOREIGN KEY (categoryID) REFERENCES categories(categoryID),
+    FOREIGN KEY (supplierID) REFERENCES suppliers(supplierID)
+);
+
+-- Product images
+CREATE TABLE product_image (
+    imageID varchar(255) PRIMARY KEY NOT NULL,
+    imageURL varchar(1000) NOT NULL,
+    productID varchar(10) NOT NULL,
+    FOREIGN KEY (productID) REFERENCES products(productID)
+);
+
+-- Cart
+CREATE TABLE cart (
+	customerID int NOT NULL,
+    productID varchar(10) NOT NULL,
+    quantity int NOT NULL,
+    timeAdd DATETIME NOT NULL,
+    PRIMARY KEY(customerID, productID),
+    FOREIGN KEY (customerID) REFERENCES customers(customerID),
+    FOREIGN KEY (productID) REFERENCES products(productID)
+);
+
+-- Receipt
+CREATE TABLE receiptP (
+    receiptPID int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    customerID int NOT NULL,
+    employeeID int NOT NULL,
+    timeBuy DATETIME NOT NULL,
+    statusR int NOT NULL,
+    FOREIGN KEY (customerID) REFERENCES customers(customerID),
+    FOREIGN KEY (employeeID) REFERENCES employees(employeeID)
+);
+
+-- Detail Receipt
+CREATE TABLE detailReceiptP (
+    receiptPID int NOT NULL,
+    productID varchar(10) NOT NULL,
+    price int(20) NOT NULL,
+    quantityBuy int(20) NOT NULL,
+    total int(30) NOT NULL,
+    PRIMARY KEY(receiptPID, productID),
+    FOREIGN KEY (receiptPID) REFERENCES receiptP(receiptPID),
+    FOREIGN KEY (productID) REFERENCES products(productID)
+);
+
---
+++ database/vpptienphong_mysql.sql
@@ -0,0 +1,315 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: localhost
+-- Thời gian đã tạo: Th4 10, 2023 lúc 07:51 AM
+-- Phiên bản máy phục vụ: 10.4.27-MariaDB
+-- Phiên bản PHP: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `vpptienphong`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `admins`
+--
+
+CREATE TABLE `admins` (
+  `adminID` int(11) NOT NULL,
+  `adminName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `cart`
+--
+
+CREATE TABLE `cart` (
+  `customerID` int(11) NOT NULL,
+  `productID` varchar(10) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `timeAdd` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `categories`
+--
+
+CREATE TABLE `categories` (
+  `categoryID` varchar(30) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `customers`
+--
+
+CREATE TABLE `customers` (
+  `customerID` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL,
+  `token` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `detailReceiptP`
+--
+
+CREATE TABLE `detailReceiptP` (
+  `receiptPID` int(11) NOT NULL,
+  `productID` varchar(10) NOT NULL,
+  `price` int(20) NOT NULL,
+  `quantityBuy` int(20) NOT NULL,
+  `total` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `employees`
+--
+
+CREATE TABLE `employees` (
+  `employeeID` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+CREATE TABLE `products` (
+  `productID` varchar(10) NOT NULL,
+  `productName` varchar(255) NOT NULL,
+  `detail` varchar(3000) NOT NULL,
+  `stock` int(20) NOT NULL,
+  `sold` int(20) NOT NULL,
+  `price` int(20) NOT NULL,
+  `status` int(5) NOT NULL,
+  `categoryID` varchar(30) NOT NULL,
+  `supplierID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `product_image`
+--
+
+CREATE TABLE `product_image` (
+  `imageID` varchar(255) NOT NULL,
+  `imageURL` varchar(1000) NOT NULL,
+  `productID` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `receiptP`
+--
+
+CREATE TABLE `receiptP` (
+  `receiptPID` int(11) NOT NULL,
+  `customerID` int(11) NOT NULL,
+  `employeeID` int(11) NOT NULL,
+  `timeBuy` datetime NOT NULL,
+  `statusR` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `suppliers`
+--
+
+CREATE TABLE `suppliers` (
+  `supplierID` int(11) NOT NULL,
+  `supplierName` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `phone` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`adminID`);
+
+--
+-- Chỉ mục cho bảng `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`customerID`,`productID`),
+  ADD KEY `productID` (`productID`);
+
+--
+-- Chỉ mục cho bảng `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`categoryID`);
+
+--
+-- Chỉ mục cho bảng `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customerID`);
+
+--
+-- Chỉ mục cho bảng `detailReceiptP`
+--
+ALTER TABLE `detailReceiptP`
+  ADD PRIMARY KEY (`receiptPID`,`productID`),
+  ADD KEY `productID` (`productID`);
+
+--
+-- Chỉ mục cho bảng `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`employeeID`);
+
+--
+-- Chỉ mục cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`productID`),
+  ADD KEY `categoryID` (`categoryID`),
+  ADD KEY `supplierID` (`supplierID`);
+
+--
+-- Chỉ mục cho bảng `product_image`
+--
+ALTER TABLE `product_image`
+  ADD PRIMARY KEY (`imageID`),
+  ADD KEY `productID` (`productID`);
+
+--
+-- Chỉ mục cho bảng `receiptP`
+--
+ALTER TABLE `receiptP`
+  ADD PRIMARY KEY (`receiptPID`),
+  ADD KEY `customerID` (`customerID`),
+  ADD KEY `employeeID` (`employeeID`);
+
+--
+-- Chỉ mục cho bảng `suppliers`
+--
+ALTER TABLE `suppliers`
+  ADD PRIMARY KEY (`supplierID`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `adminID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customerID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `employeeID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `receiptP`
+--
+ALTER TABLE `receiptP`
+  MODIFY `receiptPID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `suppliers`
+--
+ALTER TABLE `suppliers`
+  MODIFY `supplierID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customers` (`customerID`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`);
+
+--
+-- Các ràng buộc cho bảng `detailReceiptP`
+--
+ALTER TABLE `detailReceiptP`
+  ADD CONSTRAINT `detailReceiptP_ibfk_1` FOREIGN KEY (`receiptPID`) REFERENCES `receiptP` (`receiptPID`),
+  ADD CONSTRAINT `detailReceiptP_ibfk_2` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`);
+
+--
+-- Các ràng buộc cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`categoryID`) REFERENCES `categories` (`categoryID`),
+  ADD CONSTRAINT `products_ibfk_2` FOREIGN KEY (`supplierID`) REFERENCES `suppliers` (`supplierID`);
+
+--
+-- Các ràng buộc cho bảng `product_image`
+--
+ALTER TABLE `product_image`
+  ADD CONSTRAINT `product_image_ibfk_1` FOREIGN KEY (`productID`) REFERENCES `products` (`productID`);
+
+--
+-- Các ràng buộc cho bảng `receiptP`
+--
+ALTER TABLE `receiptP`
+  ADD CONSTRAINT `receiptP_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customers` (`customerID`),
+  ADD CONSTRAINT `receiptP_ibfk_2` FOREIGN KEY (`employeeID`) REFERENCES `employees` (`employeeID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
