ad5ddd58dd1870b5eebfdb9a8a6c49da35e60789
duongcongit <duongcongit@gmail.com>
2023-04-29 15:08:13 +0700

    update config

--- app/config/constants.php
+++ app/config/constants.php
@@ -2,7 +2,7 @@
     //Start Session
     session_start();
     //Create Constants to Store Non Repeating Values
-    define('SITEURL', 'http://localhost/VPPTIENPHONG-PTYCPM/');
+    define('SITEURL', 'http://localhost/vpptienphong/');
     define('LOCALHOST', 'localhost');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', '');
